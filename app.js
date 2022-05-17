35a2a08c6b6802e3766b8b86cd12f746ad073f4c
Víctor Orozco C <vaorozco@hotmail.com>
2022-05-16 23:05:10 -0600

    Consuming GET method

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const dataResponse = document.getElementsById('card');
-
-fetch('https://rickandmortyapi.com/api/character'), {
-    method: 'GET'
-}).then(res => {return res.json();})
-.then(data => {
-    
-})
-
-
-const dataResponse = document.getElementById("cards-list");
-
-
+const dataResponse = document.getElementById('cards-container');
 
 fetch('https://rickandmortyapi.com/api/character', {
-
     method: 'GET'
-
 })
-
 .then(res => {
-
     return res.json();
-
 })
-
 .then(data => {
-
-    // const characters = data.results.map((character) => `${character.name}<br>`);
-
-    // dataResponse.innerHTML = characters;
 
     let characters = data.results;
 
     characters.forEach(character => {
 
-        const card =    `<div class="card bg-dark m-1" style="width: 18rem;">
+    const card = 
+                    `<div class="card">
+                        <img src="${character.image}" alt="" class="card-image">
+                        <h3>Name: ${character.name}</h3>
+                        <h3>Status: ${character.status}</h3>
+                        <h3>Species: ${character.species}</h3>
+                        <h3>Type: ${character.type}</h3>
+                        <h3>Gender: ${character.gender}</h3>
+                        <div class="card-buttons">
+                            <i class="fa-solid fa-star card-like-button"></i>
+                            <Button class="card-add-button">Add as a player</Button>
+                        </div>
+                    </div>`
+    dataResponse.innerHTML += card;
+
+});
+
+})
+
+.catch(error => alert("ERROR"));
+
+       /* const card =    `<div class="card bg-dark m-1" style="width: 18rem;">
 
                             <img src=${character.image} class="card-img-top" alt="...">
 
@@ -60,4 +59,4 @@ fetch('https://rickandmortyapi.com/api/character', {
 
 })
 
-.catch(error => alert("ERROR"));
+.catch(error => alert("ERROR"));*/
