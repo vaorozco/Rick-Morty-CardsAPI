39a64f7655081bf80beac50a4daaafa4cedcf5f2
Víctor Orozco C <vaorozco@hotmail.com>
2022-05-24 20:15:06 -0600

    Carousel working

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
-const dataResponse = document.getElementById('test');
+const dataResponse = document.getElementById('kards-container');
+const dataCards = document.getElementById('test');
+
+/*fetch('https://rickandmortyapi.com/api/character', {
+    method: 'GET'
+})
+.then(res => {
+    return res.json();
+})
+.then(data => {
+
+    let characters = data.results;
+
+    for (let index = 0; index < 3; index++) {
+      
+    const card = 
+                    `<div class="kard">
+                        <img src="${characters[index].image}" alt="" class="kard-image">
+                        <h3>Name: ${characters[index].name}</h3>
+                        <h3>Status: ${characters[index].status}</h3>
+                        <h3>Species: ${characters[index].species}</h3>
+                        <h3>Type: ${characters[index].type}</h3>
+                        <h3>Gender: ${characters[index].gender}</h3>
+                        <div class="kard-buttons">
+                            <i class="fa-solid fa-star kard-like-button"></i>
+                            <Button class="kard-add-button">Add as a player</Button>
+                        </div>
+                    </div>`
+
+    dataResponse.innerHTML += card;
+
+};
+
+})
+
+.catch(error => alert("ERROR")); */
 
 fetch('https://rickandmortyapi.com/api/character', {
     method: 'GET'
@@ -10,57 +45,43 @@ fetch('https://rickandmortyapi.com/api/character', {
 
     let characters = data.results;
 
-    characters.forEach(character => {
-
+    for (let index = 0; index < characters.length; index++) {
+      
     const card = 
-                    // `<div class="kard">
-                    //     <img src="${character.image}" alt="" class="kard-image">
-                    //     <h3>Name: ${character.name}</h3>
-                    //     <h3>Status: ${character.status}</h3>
-                    //     <h3>Species: ${character.species}</h3>
-                    //     <h3>Type: ${character.type}</h3>
-                    //     <h3>Gender: ${character.gender}</h3>
-                    //     <div class="kard-buttons">
-                    //         <i class="fa-solid fa-star kard-like-button"></i>
-                    //         <Button class="kard-add-button">Add as a player</Button>
-                    //     </div>
-                    // </div>`
-
                     `<div class="swiper-slide card">
-            <div class="card-content">
-              <div class="image">
-                  <img src="${character.image}" alt="">
-              </div>
-  
-              <div class="media-icons">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-github"></i>
-              </div>
-  
-              <div class="name-profession">
-                <span class="${character.name}">1</span>
-                <span class="profession">Web Developer</span>
-              </div>
-  
-              <div class="rating">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="far fa-star"></i>
-                <i class="far fa-star"></i>
-              </div>
-  
-              <div class="button">
-                <button class="aboutMe">About Me</button>
-                <button class="hireMe">Hire Me</button>
-              </div>
-            </div>
-          </div>`
+                        <div class="card-content">
+                          <div class="image">
+                            <img src="${characters[index].image}" alt="">
+                          </div>
+                          
+                            <h3>${characters[index].name}</h3>
+                            <h3>Status: ${characters[index].status}</h3>
+                            <h3>Specie: ${characters[index].species}</h3>
+                            <h3>Location: ${characters[index].location.name}</h3>
+                          
+                          <div class="kard-buttons">
+                          <i class="fa-solid fa-star kard-like-button"></i>
+                            <button class="kard-add-button">Add Card</button>
+                          </div>
+                        </div>
+                      </div> `
 
-    dataResponse.innerHTML += card;
+               /* `  <div class="swiper-slide kard">
+                        <img src="${characters[index].image}" alt="" class="kard-image">
+                        <h3>Name: ${characters[index].name}</h3>
+                        <h3>Status: ${characters[index].status}</h3>
+                        <h3>Species: ${characters[index].species}</h3>
+                        <h3>Type: ${characters[index].type}</h3>
+                        <h3>Gender: ${characters[index].gender}</h3>
+                        <div class="kard-buttons">
+                            <i class="fa-solid fa-star kard-like-button"></i>
+                            <Button class="kard-add-button">Add as a player</Button>
+                        </div>
+                    </div>` */
 
-});
+    dataCards.innerHTML += card;
+
+};
 
 })
 
